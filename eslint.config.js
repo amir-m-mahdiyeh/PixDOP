67c45809478e7e7e20dd0fe454669ece6887a7a0
Amir <amir.m.mahdiyeh@gmail.com>
2026-08-08 19:43:48 +0330

    Configure ESLint and Vite and clean up starter files

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
-    },
-  },
-])
+	globalIgnores(["dist", "node_modules"]),
+	{
+		files: ["**/*.{js,jsx}"],
+		extends: [
+			js.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			globals: globals.browser,
+			parserOptions: {
+				ecmaFeatures: { jsx: true },
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
+		},
+		rules: {
+			"no-unused-vars": [
+				"warn",
+				{
+					argsIgnorePattern: "^_",
+				},
+			],
+
+			"react-hooks/exhaustive-deps": "warn",
+
+			"react-refresh/only-export-components": [
+				"warn",
+				{
+					allowConstantExport: true,
+				},
+			],
+		},
+	},
+]);
